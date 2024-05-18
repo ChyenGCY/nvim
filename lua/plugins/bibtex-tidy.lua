4f6aca488a3941590ed2dad983e8b5ba2c7889dd
Chenyu Gu <12011123@mail.sustech.edu.cn>
2024-05-18 17:24:56 +0800

    Remove none-ls for tex and delete json files.

--- lua/plugins/bibtex-tidy.lua
+++ lua/plugins/bibtex-tidy.lua
@@ -5,15 +5,15 @@ return {
       table.insert(opts.ensure_installed, "bibtex-tidy")
     end,
   },
-  {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, nls.builtins.formatting.bibtex_tidy)
-    end,
-  },
+  -- {
+  --   "nvimtools/none-ls.nvim",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     local nls = require("null-ls")
+  --     opts.sources = opts.sources or {}
+  --     table.insert(opts.sources, nls.builtins.formatting.bibtex_tidy)
+  --   end,
+  -- },
   {
     "stevearc/conform.nvim",
     optional = true,
--- lua/plugins/latexindent.lua
+++ lua/plugins/latexindent.lua
@@ -5,15 +5,15 @@ return {
       table.insert(opts.ensure_installed, "latexindent")
     end,
   },
-  {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, nls.builtins.formatting.latexindent)
-    end,
-  },
+  -- {
+  --   "nvimtools/none-ls.nvim",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     local nls = require("null-ls")
+  --     opts.sources = opts.sources or {}
+  --     table.insert(opts.sources, nls.builtins.formatting.latexindent)
+  --   end,
+  -- },
   {
     "stevearc/conform.nvim",
     optional = true,
